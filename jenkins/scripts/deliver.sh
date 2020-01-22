19304d0dbba06d31253d5141a13d203f99a63617
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2020-01-22 17:49:11 +0700

    Create deliver.sh

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,23 @@
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
+
+echo 'The following complex command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+set +x
+
+echo 'The following complex command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
+
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+scp target/${NAME}-${VERSION}.jar root@$1:/home/
+ssh root@$1 "java -jar /home/${NAME}-${VERSION}.jar"
